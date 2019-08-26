c88ed6b5ed064f7cdbcf235677fe384ed6bd3331
Erik Froese <efroese@dius.com.au>
2019-08-26 12:21:16 +1000

    pass selected recipe around

--- src/app/recipes/recipe-container/recipe-container.component.ts
+++ src/app/recipes/recipe-container/recipe-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-container',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-container.component.css']
 })
 export class RecipeContainerComponent implements OnInit {
+  selectedRecipe: Recipe;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showRecipe(recipe: Recipe) {
+    console.log(recipe);
+    this.selectedRecipe = recipe;
   }
 
 }
